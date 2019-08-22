ef37ce7ce4210d59d6e1ffc1117a9ae7c9ad9711
Nguyen Van Chinh <vanchinhpc9@gmail.com>
2019-08-22 16:29:53 +0700

    Create router for extension with value from system setting

---
+++ Controller/test/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © 2015 Inchoo d.o.o.
+ * created by Zoran Salamun(zoran.salamun@inchoo.net)
+ */
+namespace Lof\HaHelloMagento\Controller\Test;
+ 
+class Test extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * Listing all images in gallery
+     *  -@param gallery id
+     */
+    public function execute()
+    {
+        die("Lof\\HaHelloMagento\\Controller\\Test\\Test controller execute()");
+    }
+}
