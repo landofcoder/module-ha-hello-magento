7fde2acf0b06f08e0dd99a349bd750c76b896e4d
Nguyen Van Chinh <vanchinhpc9@gmail.com>
2019-08-22 10:05:36 +0700

    add extension set title and text into controler index

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,19 +4,27 @@ namespace Lof\HaHelloMagento\Controller\Index;
 class Index extends \Magento\Framework\App\Action\Action
 {
 	protected $_pageFactory;
-
+	protected $helperData;
 	public function __construct(
 		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Lof\HaHelloMagento\Helper\Data $helperData)
 	{
 		$this->_pageFactory = $pageFactory;
+		$this->helperData = $helperData;
 		return parent::__construct($context);
 	}
-
-	public function execute()
-	{
-		return $this->_pageFactory->create();
-		echo"chao moi nguoi";
-	}
+	// public function gettext(){
+	// 	return $this->helperData->getGeneralConfig('display_text');
+	// }
+	 public function execute(){
+		if($this->helperData->getGeneralConfig('enable') == 1){
+		?>
+		<head>
+			<title><?php echo $this->helperData->getGeneralConfig('title');?></title>	
+		</head>		<?php
+		}
+		return $this->_pageFactory->create();		
+	 }
 }
 
